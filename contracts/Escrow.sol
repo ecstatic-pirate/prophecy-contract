557e4687d5e779fbcdce4012d029abc6880c7f30
Remix Workspace <>
2022-10-02 19:08:25 +0200

    BETA PRE RELEASE -- Manually add admin address and update payment logic

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -10,13 +10,23 @@ contract Escrow is Ownable {
 
     using Address for address payable;
 
-    event Deposited(uint256 weiAmount);
-    event ContractorAdded(address indexed payee);
-    event AdminAdded(address indexed payee);
+    event Deposited(address indexed, uint256 weiAmount);
+    event ContractorAdded(address indexed, address indexed contractorAddress);
+    event AdminAdded(address indexed, address indexed adminAddress);
+    event ClientAdded(address indexed, address indexed clientAddress);
+    event TaskCompleted(address indexed, bool indexed inputTaskCompleted);
+    event TaskValidated(address indexed, bool indexed inputTaskValidated);
+    event AdminResolutionDone(address indexed ,bool indexed inputAdminResolution,bool indexed inputAdminDecision);
+    event ContractorPaid(uint256 indexed balancePayment);
+    event ClientPaid(uint256 indexed balancePayment);
+    event AdminPaid(uint256 indexed adminPayment);
 
     address payable private contractor;
     address payable private client;
     address payable private admin;
+
+// Activate hardcoded address later
+    // address constant private admin = payable(0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c);
 
     mapping(address => uint256) private _deposits;
 
@@ -31,6 +41,19 @@ contract Escrow is Ownable {
         _checkAdmin();
         _;
     }
+
+// Check Client Function
+// Checks if a given address is the client and only allows access to specific functions with the client address
+    
+    function _checkClient() internal view {
+        require(msg.sender == client, "caller is not the client");
+    }
+
+      modifier onlyClient() {
+        _checkClient();
+        _;
+    }
+
 
 // Check Contractor Function
 // Checks if a given address is the contractor and only allows access to specific functions with the contractor address
@@ -50,30 +73,48 @@ contract Escrow is Ownable {
  // Client sends the slected money to the smart contract
  // Can Only be called by the client. 
 
-    function deposit() public payable onlyOwner {
-        client= payable(msg.sender);
+    function deposit() public payable onlyClient {
         uint256 amount = msg.value;
-        emit Deposited(amount);
+        emit Deposited(msg.sender, amount);
     }
 
 // Add Contractor Wallet Address function
 // Client adds the wallet address of the contractor
 // Only the client can call the function
+
     
-     function addContractor(address payable payee) public onlyOwner{
-        contractor = payee;
-        emit ContractorAdded(payee);
+    uint256 private contractorState;
+    function addContractor(address payable contractorAddress) public onlyOwner{
+        require(contractorState <1 , "Contractor Address cannot be changed once added");
+        require(contractorAddress != client , "Contractor Address cannot be the same as Client Address");
+        contractor = contractorAddress;
+        contractorState += 1;
+        emit ContractorAdded(msg.sender,contractorAddress);
     }
 
 // Add Admin Wallet Address function 
 //{OPTIONAL: BETTER TO HARDCODE THE ADDRESS TO PROTECT THE FUNCTION FROM BEING MISUESD AS ADMIN HAS THE POWER TO RESOLVE PAYMENTS}
 // Client adds the wallet address of the admin
-// Only the client can call the function   
+// Only the Contract Owner(creator) can call the function   
 
-    function addAdmin(address payable payee) public {
-        admin = payee;
-        emit AdminAdded(payee);
+    function addAdmin(address payable adminAddress) public {
+        admin = adminAddress;
+        emit AdminAdded(msg.sender,adminAddress);
     }
+
+// Add Admin Wallet Address function 
+// {OPTIONAL: BETTER TO HARDCODE THE ADDRESS TO PROTECT THE FUNCTION FROM BEING MISUESD AS ADMIN HAS THE POWER TO RESOLVE PAYMENTS}
+// Client adds the wallet address of the admin
+// Only the Contract Owner(creator) can call the function   
+
+    uint256 private clientState;
+    function addClient(address payable clientAddress) public onlyOwner{
+        require(clientState <1 , "Client Address cannot be changed once added");
+        require(clientAddress != contractor , "Client Address cannot be the same as  Contractor Address");
+        client = clientAddress;
+        clientState +=1;
+        emit ClientAdded(msg.sender,clientAddress);
+    }    
 
 // Declaring status check variables
 // taskCompleted: True once the contractor marks the tasks as done
@@ -94,6 +135,7 @@ contract Escrow is Ownable {
     adminResolution = inputAdminResolution;
     adminPayContractor = inputAdminDecision;
     withdrawPayment();
+    emit AdminResolutionDone(msg.sender,inputAdminResolution,inputAdminDecision);
     }
 
 // taskCompletionStatus Function
@@ -102,7 +144,9 @@ contract Escrow is Ownable {
 // The function can only be accessed by the contractor address        
 
     function taskCompletionStatus(bool inputTaskCompleted) public onlyContractor {
+    require(address(this).balance !=0 , "Add funds to the contract");
     taskCompleted = inputTaskCompleted;
+    emit TaskCompleted(msg.sender, inputTaskCompleted);
     }
 
 // taskValidationStatus Function
@@ -110,9 +154,12 @@ contract Escrow is Ownable {
 // If true, the contractor gets paid.
 // The function can only be accessed by the client address        
 
-    function taskValidationStatus(bool inputTaskValidated) public onlyOwner{
+    function taskValidationStatus(bool inputTaskValidated) public onlyClient{
+    require(address(this).balance !=0 , "Add funds to the contract");
+    require(taskCompleted=true,"Task not completed yet");
     taskValidated = inputTaskValidated;
     withdrawPayment();
+    emit TaskValidated(msg.sender,inputTaskValidated);
     }
 
 // withdrawPayment function
@@ -122,18 +169,21 @@ contract Escrow is Ownable {
 
     function withdrawPayment() private{
     uint256 balancePayment = address(this).balance;
-    uint256 adminPayment = (balancePayment * 2)/10; //admin fee to be added based on the real time crypto value. 
+    uint256 adminPayment = (balancePayment * 2)/100; //admin fee to be added based on the real time crypto value. 
     balancePayment -= adminPayment; //updates the contract balance value
 
 
         if((taskCompleted == true && taskValidated == true)||(adminResolution == true && adminPayContractor == true)){
             contractor.sendValue(balancePayment);
             admin.sendValue(adminPayment);
+            emit ContractorPaid(balancePayment);
+            emit AdminPaid(adminPayment);
         }
         else if(adminResolution == true && adminPayContractor == false){
             client.sendValue(balancePayment);
             admin.sendValue(adminPayment);
-
+            emit ClientPaid(balancePayment);
+            emit AdminPaid(adminPayment);
             }
 
     }
